398cde3f869f7a50093b9fe6bae26e1481877049
KoleckOLP <o.kurz@atlas.cz>
2020-02-21 21:08:21 +0100

    2.1.0.5

--- main.py
+++ main.py
@@ -3,39 +3,49 @@ from call import *
 clear()
 
 if(os.path.exists(settings)):
-    print(f"yt-dl {ver} by KoleckOLP (C){year}\n")
+    name()
     loadpath()
 else:
-    firstrun()
-    
+    print("Welcome to first time setup,\n"
+         +"This program requires py-getch, youtube-dl, ffmpeg & ffprobe\n"
+         +"Do you have all the requirements? [Y/n]")
+    cmd = input("#")
+    cmd = cmd.lower() 
+    if (cmd == "y"):
+        pass
+    else:
+        print("Install all the dependencies and come back")
+        exit()
+    print("Have you installed youtube-dl with pip? [Y/n]]")
+    cmd = input("#")
+    cmd = cmd.lower() 
+    if (cmd == "y"):
+        ydpip = True
+    else:
+        ydpip = False
+    savepath(ydpip)   
 
 while(True):
-    print("\n1. Audio\n2. Video\n3. Exit\n4. Subtitles\n\n6. Update\n"+
-    "7. About & Credits\n8. Show Download path\n9. Change Download path")
+    print("\n1. Audio\n2. Video\t6. Update\n3. Exit\t\t7. Download path\n4. Subtitles\t8. About, Changelog")
     cmd = readchar("#")
 
-    if(cmd == "1"): #Audio
+    if(cmd == "1"): #audio
         audiod()
-    elif(cmd == "2"): #Video
+    elif(cmd == "2"): #video
         videod()
-    elif(cmd == "3"): #Exit (done)
+    elif(cmd == "3"): #exit
         print("See you later aligator.")
         break
-    elif(cmd == "4"): #Subtitles
+    elif(cmd == "4"): #subtitles
         subd()
-    elif(cmd == "6"): #Update (done)
+    elif(cmd == "6"): #update
         update()
-    elif(cmd == "7"): #About (done)
+    elif(cmd == "7"): #save load path
+        clear()
+        slpath()
+    elif(cmd == "8"): #about
         about()
-    elif(cmd == "8"): #showpath (done)
-        clear()
-        loadpath()
-    elif(cmd == "9"): #savepath (done)
-        clear()
-        savepath()
-        clear()
-        loadpath()
-    elif(cmd == "`"): #debug (done)
+    elif(cmd == "`"): #debug
         debug()
     else:
         clear()
