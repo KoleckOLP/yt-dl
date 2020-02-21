bc0672790e5a8b3e09acdb85da5530a1e95af52f
KoleckOLP <o.kurz@atlas.cz>
2020-02-21 10:35:31 +0100

    clean
    cleaning the repo pre python 3

--- yt-dl/bld/fullbuild.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-
-cd ..
-
-dotnet publish -r win-x64 -c Release
-
-dotnet publish -r osx-x64 -c Release
-
-dotnet publish -r linux-x64 -c Release
--- yt-dl/bld/test.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-
-cd ..
-
-dotnet publish -r win-x64 -c Debug
