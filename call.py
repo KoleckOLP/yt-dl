728e6b0d5cbad861548cef79e2a394e651482657
KoleckOLP <o.kurz@atlas.cz>
2020-02-21 22:12:22 +0100

    test

--- call.py
+++ call.py
@@ -9,7 +9,7 @@ import json
 
 
 year = datetime.now().year
-ver = "2.1.0.5 testing"
+ver = "2.1.0.5 Testing!"
 lstupdt = "21.02.2020"
 spath = sys.path[0]+os.path.sep
 settings = spath+"settings.json"
