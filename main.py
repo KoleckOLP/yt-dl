95f5174a85db00a8488bdcb1fdaf283fda67a5e7
KoleckOLP <o.kurz@atlas.cz>
2020-02-26 21:57:03 +0100

    2.1.0.6 commit 2

--- main.py
+++ main.py
@@ -6,27 +6,10 @@ if(os.path.exists(settings)):
     name()
     loadpath()
 else:
-    print("Welcome to first time setup,\n"
-         +"This program requires py-getch, youtube-dl, ffmpeg & ffprobe\n"
-         +"Do you have all the requirements? [Y/n]")
-    cmd = input("#")
-    cmd = cmd.lower() 
-    if (cmd == "y"):
-        pass
-    else:
-        print("Install all the dependencies and come back")
-        exit()
-    print("Have you installed youtube-dl with pip? [Y/n]]")
-    cmd = input("#")
-    cmd = cmd.lower() 
-    if (cmd == "y"):
-        ydpip = True
-    else:
-        ydpip = False
-    savepath(ydpip)   
+    firstrun()  
 
 while(True):
-    print("\n1. Audio\n2. Video\t6. Update\n3. Exit\t\t7. Download path\n4. Subtitles\t8. About, Changelog")
+    print("71. Audio\n2. Video\t6. Update\n3. Exit\t\t7. Download path\n4. Subtitles\t8. About, Changelog")
     cmd = readchar("#")
 
     if(cmd == "1"): #audio
---
+++ whatsnew.md
@@ -0,0 +1,4 @@
+New in 2.1.0.6
++ fixed a first time update bug
++ Changelog replaced with whats new
++ you can now delete settings
