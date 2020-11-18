1fba5d2735ff49788e1e854a363b1d22135929e5
Ondrej Kurz <kurz@fosfa.local>
2020-11-18 11:21:36 +0100

    GUI: first button handling

--- gui.py
+++ gui.py
@@ -2,10 +2,18 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from call import year, lstupdt, spath
 
+def Audio():
+    print("lol")
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("gui.ui", self)
+
+        #=====AUDIO=====#
+        self.download_button.clicked.connect(Audio)
+
+        #=====ABOUT=====#
         self.about_box.setHtml(f"<p style=\"font-size: 20px; white-space: pre\">HorseArmored inc (C){year}<br>"
                               +f"Last updated on: {lstupdt}<br>"
                               +f"My webpage: <a href=\"https://koleckolp.comli.com\">https://koleckolp.comli.com</a><br>"
@@ -13,13 +21,13 @@ class MainWindow(QtWidgets.QMainWindow):
                               +f"need help? ask here: <a href=\"https://github.com/KoleckOLP/yt-dl\">https://discord.gg/W88375j</a><br>"
                               +f"youtube-dl (C)2008-2011 Ricardo Garcia Gonzalez<br>"
                               +f"                 (C)2011-{year} youtube-dl developers<br>"
-                              +f"ffmpeg (C)2000-{year} FFmpeg team</pre><br>"
-                              +f"Change log: <a href=\"whatsnew.md\">here</a></p>")
+                              +f"ffmpeg (C)2000-{year} FFmpeg team</pre></p>")    
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
 window.show()
 app.exec_()
+
 '''
 app = QtWidgets.QApplication(sys.argv)
 
