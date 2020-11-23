bd327b1a73ee0e747cc84da22558b7ddb89f71e7
KoleckOLP-work <o.kurz@atlas.cz>
2020-11-23 15:20:55 +0100

    status bar, solo process, req, video tab, roadmap

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ from PyQt5 import QtWidgets, uic
 from call import year, lstupdt, spath, settings
 
 #==========LOAD PATH==========#
-def loadpath(s="show"):
+def loadpath(s="show"): # Taken directly from call.py, only used to load path, fail miserably if config missing or corrupted.
     global audio
     global videos
     global py
@@ -38,6 +38,11 @@ def loadpath(s="show"):
         path["Abit"]
     except KeyError:
         print("Oopsie please launch cli to fix") #if a key is not loaded from the current settings there is nothing we can do in the gui for now, lauch cli to fix
+        msg = QtWidgets.QMessageBox()
+        msg.setWindowTitle("Settings error")
+        msg.setIcon(QtWidgets.QMessageBox.Critical)
+        msg.setText("Your setting files is not correct, please run CLI version to fix")
+        msg.exec_()
     else:
         audio = path["audio"]
         videos = path["videos"]
@@ -57,76 +62,100 @@ def loadpath(s="show"):
     else:
         fdir = True
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QtWidgets.QMainWindow):    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("gui.ui", self)
+
+        def status( s=""): #shows status message and changes color of the status bar.
+            self.statusBar().showMessage(s)
+            if s == "Ready.":
+                self.statusBar().setStyleSheet("background-color: #00BB00")
+            elif s == "Busy.":
+                self.statusBar().setStyleSheet("background-color: #FF6600")
+            else:
+                self.statusBar().setStyleSheet("background-color: #A9A9A9")
+
         loadpath()
 
+        global running
+        running = False
+        status("Ready.")
+
         def Audio():
-            global process
-            self.output_console.setHtml("") #clearing the output_console
+            global running
+            if running == False:
+                running = True
+                status("Busy.")
 
-            url = self.url_bar.text()
-            if self.playlist_checkbox.isChecked():
-                numb = self.playlist_bar.text()
-            else:
-                numb = None
+                self.aud_output_console.setHtml("") #clearing the output_console
 
-            if(numb == None):
-                cmd = [["youtube-dl", "-o", f"{audio}%(title)s.%(ext)s", "--no-playlist", "-x", "--prefer-ffmpeg"],["--audio-format", "mp3", f"{url}"]]
-            elif(numb == ""):
-                cmd = [["youtube-dl", "-o", f"{audio}%(title)s.%(ext)s", "--yes-playlist", "-i", "-x", "--prefer-ffmpeg"],["--audio-format", "mp3", f"{url}"]]
-            else:
-                cmd = [["youtube-dl", "-o", f"{audio}%(title)s.%(ext)s", "--yes-playlist", "-i", "--playlist-items", f"{numb}", "-x", "--prefer-ffmpeg"],["--audio-format", "mp3", f"{url}"]]
-
-            floc = [f"--ffmpeg-location", f"{spath}"]
-            if (fdir == True):
-                cmd = cmd[0]+floc+cmd[1]
-            else:
-                cmd = cmd[0]+cmd[1]
-
-            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=0x08000000)
-            for line in itertools.chain(process.stdout, process.stderr): 
-                lol = window.isVisible()
-                if lol == False: #if window of the app was closed kill the subrocess.
-                    process.terminate()
+                url = self.aud_url_bar.text()
+                if self.aud_playlist_checkbox.isChecked():
+                    numb = self.aud_playlist_bar.text()
                 else:
-                    line = str(line)
-                    line = line[2:-1]
-                    if "\\n" in line:
-                        line = line.replace("\\n", "\n")
-                    if "\\r" in line:
-                        line = line.replace("\\r", "\n")
-                    if "\\\\" in line:
-                        line = line.replace("\\\\","\\")
-                    if "\\'" in line:
-                        line = line.replace("\\'","'")
-                    self.output_console.insertPlainText(line)
-                    QtWidgets.QApplication.processEvents()
-                    self.scrollbar = self.output_console.verticalScrollBar()
-                    self.scrollbar.setValue(self.scrollbar.maximum())
-                    QtWidgets.QApplication.processEvents()
-            
-            print("\a")
-            process = None
-            self.output_console.insertPlainText("Command Execution ended.")
-            QtWidgets.QApplication.processEvents()
-            self.scrollbar = self.output_console.verticalScrollBar()
-            self.scrollbar.setValue(self.scrollbar.maximum())
-            QtWidgets.QApplication.processEvents()
+                    numb = None
+
+                if(numb == None):
+                    cmd = [["youtube-dl", "-o", f"{audio}%(title)s.%(ext)s", "--no-playlist", "-x", "--prefer-ffmpeg"],["--audio-format", "mp3", f"{url}"]]
+                elif(numb == ""):
+                    cmd = [["youtube-dl", "-o", f"{audio}%(title)s.%(ext)s", "--yes-playlist", "-i", "-x", "--prefer-ffmpeg"],["--audio-format", "mp3", f"{url}"]]
+                else:
+                    cmd = [["youtube-dl", "-o", f"{audio}%(title)s.%(ext)s", "--yes-playlist", "-i", "--playlist-items", f"{numb}", "-x", "--prefer-ffmpeg"],["--audio-format", "mp3", f"{url}"]]
+
+                floc = [f"--ffmpeg-location", f"{spath}"]
+                if (fdir == True):
+                    cmd = cmd[0]+floc+cmd[1]
+                else:
+                    cmd = cmd[0]+cmd[1]
+
+                process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=0x08000000)
+                for line in itertools.chain(process.stdout, process.stderr): 
+                    gui = window.isVisible()
+                    if gui == False: #if window of the app was closed kill the subrocess.
+                        process.terminate()
+                    else:
+                        line = str(line)
+                        line = line[2:-1]
+                        if "\\n" in line:
+                            line = line.replace("\\n", "\n")
+                        if "\\r" in line:
+                            line = line.replace("\\r", "\n")
+                        if "\\\\" in line:
+                            line = line.replace("\\\\","\\")
+                        if "\\'" in line:
+                            line = line.replace("\\'","'")
+                        self.aud_output_console.insertPlainText(line)
+                        QtWidgets.QApplication.processEvents()
+                        self.scrollbar = self.aud_output_console.verticalScrollBar()
+                        self.scrollbar.setValue(self.scrollbar.maximum())
+                
+                print("\a")
+                self.aud_output_console.insertPlainText("Process has finished.")
+                QtWidgets.QApplication.processEvents()
+                self.scrollbar = self.aud_output_console.verticalScrollBar()
+                self.scrollbar.setValue(self.scrollbar.maximum())
+                running = False
+                status("Ready.")
+            else:
+                msg = QtWidgets.QMessageBox()
+                msg.setWindowTitle("Process warning")
+                msg.setIcon(QtWidgets.QMessageBox.Warning)
+                msg.setText("One process already running!")
+                msg.exec_()
                 
         def playlist_bar_enable():
-            self.playlist_bar.setEnabled(self.playlist_checkbox.isChecked())
-            if(self.playlist_checkbox.isChecked()):
-                self.playlist_bar.setStyleSheet("background-color: #909090;")
+            self.aud_playlist_bar.setEnabled(self.aud_playlist_checkbox.isChecked())
+            if(self.aud_playlist_checkbox.isChecked()):
+                self.aud_playlist_bar.setStyleSheet("background-color: #909090;")
             else:
-                self.playlist_bar.setStyleSheet("background-color: #707070;")
+                self.aud_playlist_bar.setStyleSheet("background-color: #707070;")
 
         #=====AUDIO=====#
-        self.download_button.clicked.connect(Audio)
-        self.playlist_checkbox.clicked.connect(playlist_bar_enable)
-        self.output_console.setHtml("Welcome to yt-dl-gui paste a link and hit download.")
+        self.aud_download_button.clicked.connect(Audio)
+        self.aud_playlist_checkbox.clicked.connect(playlist_bar_enable)
+        self.aud_output_console.setHtml("Welcome to yt-dl-gui paste a link and hit download.")
 
         #=====ABOUT=====#
         self.about_box.setHtml(f"<p style=\"font-size: 20px; white-space: pre\">HorseArmored inc (C){year}<br>"
--- testing.md
+++ testing.md
@@ -1,18 +1,22 @@
 ========================================
 ### Issue Tracking / To Do List
-
+2.1.8-gui:
 #       status severity         the issue description                                                   possible solution                                               affected OS
 .#1     [done] (highest)        Make sure subprocess is killed when gui dies                            Loop is cheking if gui lives if not kills subprocess            all
 .#2     [done] (highest)        Subprocess output to a TextBox                                          Process events after every single line, see .#6                 all
 .#3     [done] (high)           Audio download                                                          Somewhat works                                                  all
-.#4     [    ] (high)           You can start subpocess more than once download button                  Bool variable that says if subprocess is running already        all
+.#4     [done] (high)           You can start subpocess more than once download button                  Shown in the status, will throw a message box                   all
 .#5     [    ] (medium)         Remember to save download path with double separator at the end         * Check path saving more carefully                              Windows
-.#6     [    ] (medium)         UI runs really slowly while subprocess writing to TextBox               * ProcessingEvents twice a line may be a bad idea               all
+.#6    *[done] (medium)         UI runs really slowly while subprocess writing to TextBox               * I don't think I can fix that so I show satus                  all
 .#7     [done] (medium)         Link on a TextBox was opening in TextBox instead of external app        Allowing openExternalLinks                                      all
 .#8     [done] (medium)         Get over initial hurdles of QtDesigner, Qt & window resizing            Reading testing                                                 all
 .#9     [done] (medium)         ffapps in directory gui support subprocess                              List magics                                                     all
 .#10    [done] (low)            Change UI colors                                                        More QtDesigner learning                                        all
+.#11    [    ] (low)            Download in a specific tab could be shown by * or color of tab          Starting audio download would change tab name                   all
 .#999   [    ] (low)            Kali: Ctrl+D to quit                                                    * Might actually work in Qt                                     Linux
+
+2.1.9-gui_v2:
+.#1     [   ] (medium)          Less Tabs design audio, vides, sub radio buttons                        Migth be averall better option.                                 all
 
 ========================================
 ### GitHub
@@ -44,3 +48,10 @@ updating submodule, "cd submodule", write update "add",  "commit"(update to subm
 
 yt-dl in PowerShell in Windows Terminal
 "wt pwsh D:\Programs\1.NoInstall\yt-dl\yt-dl.ps1"
+
+========================================
+### Random stuff I need to write down
+
+video url,
+playlist
+quality
