973bf471f9b5c152d9b46796d0d359fa391a8d7f
KoleckOLP-work <o.kurz@atlas.cz>
2021-04-26 14:51:06 +0200

    Forgot to update release info I am still not used to working from PyCharm.

--- release.py
+++ release.py
@@ -2,9 +2,9 @@ import sys, os
 from datetime import datetime
 
 year = datetime.now().year
-lstupdt = "2021-04-13" #Well not that it's a separete file I should be updating it more.
+lstupdt = "2021-04-26" #Well not that it's a separete file I should be updating it more.
 spath = sys.path[0]+os.path.sep #path of the yt-dl dir
 settigui5 = spath+"gui5_settings.json"
 setticli = spath+"cli_settings.json"
 curb = "master"
-ver = "2.1.8"
+ver = "2.1.8.1"
