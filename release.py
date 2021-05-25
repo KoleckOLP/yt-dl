e4ed55723076bc075a316d115d9d1ff05e17285c
KoleckOLP-work <o.kurz@atlas.cz>
2021-05-25 09:14:45 +0200

    Fixing issues found by PyCharm

--- release.py
+++ release.py
@@ -2,8 +2,8 @@ import sys, os
 from datetime import datetime
 
 year = datetime.now().year
-lstupdt = "2021-05-21" #Well not that it's a separete file I should be updating it more.
-spath = sys.path[0]+os.path.sep #path of the yt-dl dir
+lstupdt = "2021-05-21"  # Well not that it's a separate file I should be updating it more.
+spath = sys.path[0]+os.path.sep  # path of the yt-dl dir
 settigui5 = spath+"gui5_settings.json"
 setticli = spath+"cli_settings.json"
 curb = "temp-test"
