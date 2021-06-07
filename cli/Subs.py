7c8ea7097a68eb230cfe99d407cdb95bb326e141
KoleckOLP-work <o.kurz@atlas.cz>
2021-06-07 12:52:22 +0200

    Split Subs
    This brings cli uptodate,
    Fixes to of issues (some may still exist)
    and ads working ~ish~ playlist, kinda scuffed may fix one day who knows
    And add language option all to download all subs.

--- cli/Subs.py
+++ cli/Subs.py
@@ -1,43 +1,61 @@
 import os
-import glob
-import tempfile
 # Imports from this project
-from release import spath
 from kolreq.kolreq import clear
+from shared.Subs import subs_list_shared, subs_shared_part1, subs_shared_part2, subs_shared_part3
 
 
 def Subs(call):
-    if call.fdir:
-        floc = f"--ffmpeg-location {spath}"
-    else:
-        floc = ""
+    lang = ""
 
     clear()
-    temp = tempfile.mkdtemp() + os.path.sep
-    print("link to video with subs or 0. GoBack")
+    print("link to video, playlist, 0. GoBack")
     url = input("#")
     if (url == "0"):
         clear()
         call.name()
     else:
-        print("<Enter> to download default sub (en),\n" +
-              "1 to choose language")
+        print("<Enter> a single video, \n" +
+              "1. to download full playlist or follow example 1-3,7,9")
         numb = input("#")
-        if(numb == ""):
-            lnk = f"-o \"{temp}%(title)s.%(ext)s\" --no-playlist --write-sub --write-auto-sub --sub-format vtt --skip-download --prefer-ffmpeg {floc} \"{url}\""
+        if (numb == "1"):  # fix for playlist
+            items = ""
         else:
-            print("starting youtube-dl please wait...")
-            os.system(f"youtube-dl --list-subs --noplaylist {url}")
-            print("choose sub language")
-            numb = input("#")
-            lnk = f"-o \"{temp}%(title)s.%(ext)s\" --no-playlist --write-sub --write-auto-sub --sub-lang \"{numb}\" --sub-format vtt --skip-download --prefer-ffmpeg {floc} \"{url}\""
+            items = numb
+
+        if (numb == ""):  # no playlist
+            print("<Enter> default lang (probably en)\n" +
+                  "1. list languages")
+            use_custom_lang = input("#")
+            if use_custom_lang == "1":
+                cmd = subs_list_shared(url)
+                call.process_start(cmd)
+                print("choose language of the subtitles by typing it's code")
+                lang = input("#")
+        else:  # playlist
+            pass
+
+        cmd, temp = subs_shared_part1(url, bool(numb), items, lang, call.floc)
+
         print("starting youtube-dl please wait...")
-        os.system("youtube-dl  " + lnk)
-        pie = glob.glob(f"{temp}*.vtt")
-        cream = os.path.basename(pie[0])
-        cream = cream[:-3]
-        lick = f"{call.settings.Youtubedl.videoDir}{cream}srt"  # I don't like this fix to a complain about var type
-        os.makedirs(call.settings.Youtubedl.videoDir, exist_ok=True)
-        print("starting youtube-dl please wait...")
-        os.system(f"ffmpeg -i \"{pie[0]}\" \"{lick}\"")
-        print("\a")
+
+        call.process_start(cmd)
+
+        subpath, newsubpath = subs_shared_part2(temp.name+os.path.sep, call.settings.Youtubedl.videoDir)
+
+        FfmpegLines = subs_shared_part3(call, subpath, newsubpath)
+
+        if isinstance(FfmpegLines, str):
+            if FfmpegLines == "error":
+                print(f"#yt-dl# found an issue aborting...\n")
+            else:
+                print(f"#yt-dl# file {FfmpegLines} already exists aborting...\n")
+
+        else:
+            for line in FfmpegLines:
+                print("#yt-dl# starting ffmpeg please wait...\n")
+
+                call.process_start(line)
+
+                print("\a")
+
+        temp.cleanup()
