334207c9996a1aca02690ade50cd0ecf64cd579b
KoleckOLP-work <o.kurz@atlas.cz>
2021-07-30 07:49:29 +0200

    Change branch

--- release.py
+++ release.py
@@ -8,5 +8,5 @@ spath = sys.path[0]+os.path.sep  # path of the yt-dl dir
 settingsPath = spath+"settings.json"
 videoDirDefault = spath+"videos"+os.path.sep
 audioDirDefault = spath+"audio"+os.path.sep
-curb = "major-refactor"
+curb = "testing"
 ver = "2.2.0.0"
