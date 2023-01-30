4474345e5c817eecfc9351e24711617ec83e7523
koleq(WORK-D) <o.kurz@atlas.cz>
2023-01-30 08:14:06 +0100

    Version Bump

--- release.py
+++ release.py
@@ -2,10 +2,10 @@ import os
 from datetime import datetime
 
 year = datetime.now().year
-lstupdt = "2022-12-02"  # Well now that it's in a separate file I should be updating it more often.
+lstupdt = "2023-01-30"  # Well now that it's in a separate file I should be updating it more often.
 spath = os.getcwd()+os.path.sep  # sys.path[0]+os.path.sep  # path of the yt-dl dir
 settingsPath = spath+"settings.json"
 videoDirDefault = spath+"videos"+os.path.sep
 audioDirDefault = spath+"audio"+os.path.sep
 curb = "testing"
-ver = "2.2.1.2"  # 2. python(language), 2. major(gigantic), 1. minor(big), 1 hotfix(small)
+ver = "2.2.1.3"  # 2. python(language), 2. major(gigantic), 1. minor(big), 1 hotfix(small)
