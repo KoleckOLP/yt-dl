8d28e77396f10c31c71562584ef98f9128263555
koleq(NICOLEv2) <o.kurz@atlas.cz>
2024-05-01 14:03:48 +0200

    copy to clipboard only in normal qual, and add quote filter

--- gui/Video.py
+++ gui/Video.py
@@ -35,26 +35,27 @@ def Video(window):
 
     process_output(window, window.vid_output_console, window.vid_download_button, window.process)
 
-    #attempt putting the downloaded video into the clipboard
-    latest_file = max(glob.glob(f"{window.settings.Youtubedl.videoDir}*"), key=os.path.getctime)
+    if window.vid_normal_radio.isChecked(): #only ty to put video in clipboard if it's normal quality
+        #attempt putting the downloaded video into the clipboard
+        latest_file = max(glob.glob(f"{window.settings.Youtubedl.videoDir}*"), key=os.path.getctime)
+        latest_file = latest_file.replace("‘", "*")  # this character makes set-clipboard fail, and prolly is not the only one
+        Powershell_process = subprocess.run(
+            ['powershell', 'Set-Clipboard', '-Path',  f'\'{latest_file}\''], 
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True
+        )
 
-    Powershell_process = subprocess.run(
-        ['powershell', 'Set-Clipboard', '-Path',  f'\'{latest_file}\''], 
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True
-    )
+        # Check the exit status
+        if Powershell_process.returncode != 0:
+            with open("log.txt", "a", encoding="utf8") as f:
+                f.write(f"{datetime.datetime.now()}\nvideo url: {window.vid_url_bar.text()}\n")
+                f.write(f"Command '{Powershell_process.args}' returned non-zero exit status {Powershell_process.returncode}.\n")
+                f.write(Powershell_process.stdout)
+                f.write(Powershell_process.stderr)
 
-    # Check the exit status
-    if Powershell_process.returncode != 0:
-        with open("log.txt", "a", encoding="utf8") as f:
-            f.write(f"{datetime.datetime.now()}\nvideo url: {window.vid_url_bar.text()}\n")
-            f.write(f"Command '{Powershell_process.args}' returned non-zero exit status {Powershell_process.returncode}.\n")
-            f.write(Powershell_process.stdout)
-            f.write(Powershell_process.stderr)
-
-    if (window.settings.autoClose):
-        exit()  # problably not the cleanest solution but doesn't left processes behind
+        if (window.settings.autoClose):
+            exit()  # problably not the cleanest solution but doesn't left processes behind
 
 
 def vid_quality(window):
