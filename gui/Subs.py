20ac7f0465d834fea342ed30734941d5023ccce8
koleq(NICOLEv2) <o.kurz@atlas.cz>
2025-06-24 19:21:09 +0200

    Subs has to be blocking like Update

--- gui/Subs.py
+++ gui/Subs.py
@@ -1,7 +1,7 @@
 import os, sys
 # Imports from this project
 from shared.Subs import subs_shared_list, subs_shared_download, subs_shared_paths_for_ffmpeg, subs_shared_lines_for_ffmpeg
-from gui.Process import process_start, process_output_threaded
+from gui.Process import process_start, process_output
 from gui.Settings import set_save
 
 
@@ -21,27 +21,26 @@ def Subs(window):
 
     window.process = process_start(window, cmd, window.sub_output_console, window.sub_download_button, window.process)
 
-    thread = process_output_threaded(window, window.sub_output_console, window.sub_download_button, window.process)
-    window.thread = thread
+    process_output(window, window.sub_output_console, window.sub_download_button, window.process)
 
     subpath, newsubpath = subs_shared_paths_for_ffmpeg(temp.name + os.path.sep, window.settings.Ytdlp.videoDir)
 
     FfmpegLines = subs_shared_lines_for_ffmpeg(window, subpath, newsubpath)
+
+    print(FfmpegLines)
 
     if isinstance(FfmpegLines, str):
         if FfmpegLines == "error":
             window.sub_output_console.insertPlainText(f"#yt-dl# found an issue aborting...\n")
         else:
             window.sub_output_console.insertPlainText(f"#yt-dl# file {FfmpegLines} already exists aborting...\n")
-
     else:
         for line in FfmpegLines:
             window.sub_output_console.insertPlainText("#yt-dl# starting ffmpeg please wait...\n")
 
             window.process = process_start(window, line, window.sub_output_console, window.sub_download_button, window.process)
 
-            thread = process_output_threaded(window, window.sub_output_console, window.sub_download_button, window.process)
-            window.thread = thread
+            process_output(window, window.sub_output_console, window.sub_download_button, window.process)
 
     temp.cleanup()
 
