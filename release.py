367281158dddace5ea47d786e2dc35c2e99005db
koleq(ANNAs) <o.kurz@atlas.cz>
2026-01-21 13:34:11 +0100

    version bump

--- release.py
+++ release.py
@@ -7,10 +7,5 @@ spath = os.getcwd()+os.path.sep  # sys.path[0]+os.path.sep  # path of the yt-dl 
 settingsPath = spath+"settings.json"
 videoDirDefault = spath+"videos"+os.path.sep
 audioDirDefault = spath+"audio"+os.path.sep
-<<<<<<< HEAD
-curb = "refactor/snake_case_naming"
-ver = "2.2.3.3"  # 2. python(language), 2. major(gigantic), 1. minor(big), 1 hotfix(small)
-=======
-curb = "Threading3"
-ver = "2.2.4.0-dev3"  # 2. python(language), 2. major(gigantic), 1. minor(big), 1 hotfix(small)
->>>>>>> c40dbbf (Fixing Windows 8.1 again...)
+curb = "AI_Refactor"
+ver = "2.2.4.0"  # 2. python(language), 2. major(gigantic), 1. minor(big), 1 hotfix(small)
