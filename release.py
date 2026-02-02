519e294ba73d48c1b6aed32a62ea0fd589dd2962
koleq(ANNAs) <o.kurz@atlas.cz>
2026-02-02 11:05:31 +0100

    Vesion Bump

--- release.py
+++ release.py
@@ -2,10 +2,10 @@ import os
 from datetime import datetime
 
 year = datetime.now().year
-lstupdt = "2026-01-21"  # Well now that it's in a separate file I should be updating it more often.
+lstupdt = "2026-02-02"  # Well now that it's in a separate file I should be updating it more often.
 spath = os.getcwd()+os.path.sep  # sys.path[0]+os.path.sep  # path of the yt-dl dir
 settingsPath = spath+"settings.json"
 videoDirDefault = spath+"videos"+os.path.sep
 audioDirDefault = spath+"audio"+os.path.sep
 curb = "AI_Refactor"
-ver = "2.2.4.0"  # 2. python(language), 2. major(gigantic), 1. minor(big), 1 hotfix(small)
+ver = "2.2.4.1"  # 2. python(language), 2. major(gigantic), 1. minor(big), 1 hotfix(small)
